782f2518f29afe8f97e0069dfe59b758db4cf0ea
kpcyrd <git@rxv.cc>
2014-10-19 20:05:39 +0200

    Add "Add client" to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var add_peer = require('./routes/add');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.put('/add/:id', add_peer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

c324730800fc98ec9895e9e6104ddd5fd62260f8
kpcyrd <git@rxv.cc>
2014-10-25 19:01:33 +0200

    Add add handler

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.put('/add/:id', add_peer);
+app.post('/add', add_peer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/add.js
+++ routes/add.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
+router.post('/add', function(req, res) {
   res.send('respond with a resource');
 });
 

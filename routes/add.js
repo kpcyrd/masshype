54eaa457f55ab14e1aff7b10e2869a57316022bb
kpcyrd <git@rxv.cc>
2014-10-25 19:28:24 +0200

    Generate password for peer

--- routes/add.js
+++ routes/add.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var fs = require('fs');
+var pwgen = require('password-generator');
+
+function getFileName(email) {
+    var hash = crypto.createHash('sha256');
+    hash.update(email);
+    return hash.digest('hex');
+}
 
 router.post('/add', function(req, res) {
-    var hash = crypto.createHash('sha256');
     var email = req.body.email;
-    hash.update(email);
-    var filename = hash.digest('hex');
+    var pw = pwgen(31);
 
-    var data = JSON.stringify({email: email});
+    var filename = getFileName(email);
+
+    var data = JSON.stringify({email: email, password: pw});
 
     fs.writeFile('data/' + filename + '.json', data, function(err) {
         if(err) {

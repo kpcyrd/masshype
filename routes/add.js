e3c10cbaf092885d212592e1d2f84fdb5fadec50
kpcyrd <git@rxv.cc>
2014-10-25 19:21:47 +0200

    Allow adding of peers

--- routes/add.js
+++ routes/add.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var fs = require('fs');
 
 router.post('/add', function(req, res) {
-  res.send('respond with a resource');
+    var hash = crypto.createHash('sha256');
+    var email = req.body.email;
+    hash.update(email);
+    var filename = hash.digest('hex');
+
+    var data = JSON.stringify({email: email});
+
+    fs.writeFile('data/' + filename + '.json', data, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Masshype' });
+
+    fs.readdir('data/', function(err, files) {
+        var nodes = [];
+
+        files.forEach(function(a) {
+            if(/.json$/.test(a))
+                nodes.push(require('../data/' + a));
+        });
+
+        res.render('index', { title: 'Masshype', nodes: nodes });
+    });
 });
 
 module.exports = router;
